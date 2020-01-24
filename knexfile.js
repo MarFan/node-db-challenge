ce3bb5b222b113105f762f758ae338fe941dab4e
Mr. Jonah <jaitch@gmail.com>
2020-01-24 11:00:03 -0600

    Data Schema Built

---
+++ knexfile.js
@@ -0,0 +1,26 @@
+// Update with your config settings.
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    client: 'mysql2',
+    connection: {
+      host: process.env.DB_HOST,
+      user:     process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME
+    },
+    pool: {
+      min: 0,
+      max: 7
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  }
+
+};
